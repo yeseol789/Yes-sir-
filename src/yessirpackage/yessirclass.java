4c716c9e57b1fdb26995bcdd0f1e80b8aadb3aac
내 문서 < @PC>
2019-02-28 11:49:45 +0900

    second hello world plus

--- src/yessirpackage/yessirclass.java
+++ src/yessirpackage/yessirclass.java
@@ -2,6 +2,7 @@ package yessirpackage;
 
 public class yessirclass {public static void main(String[] args) {
 	 System.out.println("Hello world")
+	 System.out.println("second hello word");
 		
 }
 	
