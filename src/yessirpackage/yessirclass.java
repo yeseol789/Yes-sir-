104da181633eb710c16d130edc8db610ad6a7c3d
내 문서 < @PC>
2019-02-28 20:18:43 +0900

    추가

--- src/yessirpackage/yessirclass.java
+++ src/yessirpackage/yessirclass.java
@@ -3,7 +3,7 @@ package yessirpackage;
 public class yessirclass {public static void main(String[] args) {
 	 System.out.println("Hello world")
 	 System.out.println("second hello world");
-		System.out.println("third hello world!");
+		System.out.println("third hello world!?");
 }
 	
 }
